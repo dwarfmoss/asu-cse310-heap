a59215a66a552d99d2ddc059a41bf0f8685002f6
Steve Lloyd <dwarfmoss@gmail.com>
2021-03-15 15:05:01 -0700

    Remove the reprinting of the commands

--- main.cpp
+++ main.cpp
@@ -14,11 +14,10 @@ int main() {
         c = nextCommand(&i, &f, &v);
         switch (c) {
         case 's':
-        case 'S':   printf("COMMAND: %c\n", c); exit(0);
+        case 'S':   exit(0);
 
         case 'p':
-        case 'P':   printf("COMMAND: %c\n", c);
-                    if (heap) {
+        case 'P':   if (heap) {
                         heap->PrintHeap();
                     } else {
                         printf("Error: cannot print\n");
@@ -26,8 +25,7 @@ int main() {
                     break;
 
         case 'w':
-        case 'W':   printf("COMMAND: %c\n", c);
-                    if (heap) {
+        case 'W':   if (heap) {
                         heap->WriteHeap();
                     } else {
                         printf("Error: cannot write\n");
@@ -36,18 +34,15 @@ int main() {
                     break;
 
         case 'c':
-        case 'C':   printf("COMMAND: %c %d\n", c, v);
-                    heap = new Heap(v);
+        case 'C':   heap = new Heap(v);
                     break;
 
         case 'r':
-        case 'R':   printf("COMMAND: %c %d\n", c, f);
-                    ReadFile(heap, f);
+        case 'R':   ReadFile(heap, f);
                     break;
 
         case 'i':
-        case 'I':   printf("COMMAND: %c %d\n", c, v);
-                    if (!heap || heap->size == heap->capacity) {
+        case 'I':   if (!heap || heap->size == heap->capacity) {
                         printf("Error: heap is NULL or full\n");
                     } else {
                         heap->Insert(v);
@@ -55,8 +50,7 @@ int main() {
                     break;
 
         case 'd':
-        case 'D':   printf("COMMAND: %c %d\n", c, f);
-                    if (!heap || heap->size == 0) {
+        case 'D':   if (!heap || heap->size == 0) {
                         printf("Error: heap is NULL or empty\n");
                     } else {
                         heap->ExtractMin(f);
@@ -64,8 +58,7 @@ int main() {
                     break;
 
         case 'k':
-        case 'K':   printf("COMMAND: %c %d %d\n", c, i, v);
-                    if (!heap || i < 0 || i > heap->size - 1 || v >= heap->H[i].key) {
+        case 'K':   if (!heap || i < 0 || i > heap->size - 1 || v >= heap->H[i].key) {
                         printf("Error: invalid call to DecreaseKey\n");
                     } else {
                         heap->DecreaseKey(i, v);
